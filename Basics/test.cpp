90ce17ee26d80ca3898d8d66a8c9410957fdca83
TechhManager <tech@inside.com>
2025-11-25 18:17:30 +0530

    organizing folders

---
+++ Basics/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int doubleNumber(int x){
+    
+    return x*2;
+}
+
+int main(){
+
+    int x{};
+
+    std::
+    cin >> x;
+
+    std::cout << doubleNumber(x) << '\n';
+
+    return 0;
+}
